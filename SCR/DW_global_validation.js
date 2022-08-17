d4a395d008bcee9a6a84f9efd4781b188530997a
zanderVenter <34233488+zanderVenter@users.noreply.github.com>
2022-08-17 11:18:00 +0200

    Added comments to script

--- SCR/DW_global_validation.js
+++ SCR/DW_global_validation.js
@@ -1,57 +1,3 @@
-var geometry = 
-    /* color: #d63000 */
-    /* shown: false */
-    /* displayProperties: [
-      {
-        "type": "rectangle"
-      }
-    ] */
-    ee.Geometry.Polygon(
-        [[[23.16838006397435, 50.24329062955167],
-          [23.16838006397435, 43.50228942242464],
-          [32.506758970224354, 43.50228942242464],
-          [32.506758970224354, 50.24329062955167]]], null, false),
-    geometry2 = 
-    /* color: #d63000 */
-    /* shown: false */
-    /* displayProperties: [
-      {
-        "type": "rectangle"
-      }
-    ] */
-    ee.Geometry.Polygon(
-        [[[18.881128124059114, -33.93183438425864],
-          [18.881128124059114, -33.94337025730755],
-          [18.895891002477082, -33.94337025730755],
-          [18.895891002477082, -33.93183438425864]]], null, false),
-    geometry3 = 
-    /* color: #d63000 */
-    /* shown: false */
-    /* displayProperties: [
-      {
-        "type": "rectangle"
-      }
-    ] */
-    ee.Geometry.Polygon(
-        [[[-13.582928709575972, 70.93095515747594],
-          [-13.582928709575972, 30.97748214423164],
-          [38.448321290424026, 30.97748214423164],
-          [38.448321290424026, 70.93095515747594]]], null, false),
-    geometry4 = 
-    /* color: #98ff00 */
-    /* shown: false */
-    /* displayProperties: [
-      {
-        "type": "rectangle"
-      }
-    ] */
-    ee.Geometry.Polygon(
-        [[[-55.464199777573945, -7.037106333152468],
-          [-55.464199777573945, -7.089747131181427],
-          [-55.408753152818086, -7.089747131181427],
-          [-55.408753152818086, -7.037106333152468]]], null, false);
-
-
 // Author: Zander Venter - zander.venter@nina.no
 
 // Workflow in this script:
@@ -65,10 +11,13 @@ var geometry =
   // Setup global environment ///////////////////////////////////////////////////////////////////////////////
 */
 
+// Set the satellite imagery with labels as the default basemap
 Map.setOptions('HYBRID');
 
+// Create a single polygon with a global extent
 var globalBounds = ee.Geometry.Polygon([-180, 90, 0, 90, 180, 90, 180, -90, 10, -90, -180, -90], null, false);
 
+// Create a random image that is then converted to a (square) grid
 var globalSquareGrid = ee.Image.random(123).multiply(1e6).toInt()
     .reduceToVectors({
       reducer: ee.Reducer.countEvery(),
@@ -78,8 +27,11 @@ var globalSquareGrid = ee.Image.random(123).multiply(1e6).toInt()
       scale: 2500000,
       crs: 'EPSG:4326'
     });
+// Visualise Global grid
 Map.addLayer(globalSquareGrid, {}, 'globalSquareGrid', 0)
 
+// Defining a single color palette (as a dictionary) for visualising the three global lulc products.
+// Keys correspond to 9 lulc classes and values correspond to hex colour codes (used as palette later)
 var lcDict = {
   labels: [
     "Build area", //1
@@ -99,12 +51,13 @@ var lcDict = {
     '#88B053',
     '#DFC35A',
     '#397D49',
-    '#f2bac1', // '#7A87C6'
-    '#0002f6', // '#419BDF'
-    '#6fdbde' // #B39FE1'
+    '#f2bac1', 
+    '#0002f6',
+    '#6fdbde'
   ]
 }
 
+// Defines vis params in a format expected by GEE when using map.addlayer.
 var lcVizParams = {
   min: 1,
   max: 9,
@@ -128,17 +81,18 @@ Map.addLayer(globalHexGrid, {}, 'globalGrid', 0)
 var validation = ee.ImageCollection('projects/nina/GIS_synergy/Extent/DW_global_validation_tiles')
   .select([1], ['grouhdtruth']);
 
-// Reclassify typology
+// Reclassify typology for dynamic world validation data (above)
 validation = validation.map(function(i){
   return i.updateMask(i.neq(0)).subtract(1).remap({
     from:[0,1,2,3,4,5,6,7,8],
     to:  [8,6,4,7,2,5,1,3,9]
-  }).copyProperties(i)
+  }).copyProperties(i)//copy metadata from each image
 })
 
 //Map.centerObject(validation.first())
 Map.addLayer(validation.mean(), lcVizParams, 'validation', 0)
 
+//get validation centroids for dynamic world validation tiles. set unique id for each centroid
 var validationCentroids = validation.map(function(i){
   return ee.Feature(i.geometry().centroid(100), {
     S2_GEE_ID: i.get('S2_GEE_ID'), dw_id: i.get('dw_id')
@@ -158,6 +112,7 @@ Map.addLayer(lucas, {}, 'lucas',0);
 
 //// ESRI 2020 ------------------------------------------------------------------------
 // From here: https://samapriya.github.io/awesome-gee-community-datasets/projects/esrilc2020/
+// load and reclassify esri global lulc data to common typology
 var esri = ee.ImageCollection("projects/sat-io/open-datasets/landcover/ESRI_Global-LULC_10m").mosaic();
 esri = esri.remap({
   from: [1,2,3,4,5,6,7,8,9,10],
@@ -167,20 +122,18 @@ Map.addLayer(esri, lcVizParams, 'esri',0)
 
 
 //// World cover ------------------------------------------------------------------
+// load and reclassify esri global lulc data to common typology
 var worldcover = ee.ImageCollection("ESA/WorldCover/v100").first().rename('wc');
 worldcover = worldcover.divide(10).remap({
   from: [1,2,3,4,5,6,7,8,9,9.5,10],
   to:   [6,5,4,2,1,3,9,8,7,7,3]
 });
-worldcover = worldcover.updateMask(esri)
 Map.addLayer(worldcover, lcVizParams, 'worldcover',0)
 
 
 //// Dynamic World ------------------------------------------------------------------------
 // Pre-exported annual mosaic for 2020 from script "DW_global_mosaic_2020.js"
-var dynamicWorld = ee.ImageCollection('projects/nina/GIS_synergy/Extent/DW_global_2020')
-  .merge(ee.ImageCollection('users/rangelandee/DW_global_2020'))
-  .merge(ee.ImageCollection('users/grazingresearch/DW_global_2020'))
+var dynamicWorld = ee.ImageCollection('projects/nina/GIS_synergy/Extent/DW_global_2020_mode')
   .map(function(i){
     var imgLabel = i.remap({
       from:[0,1,2,3,4,5,6,7,8],
@@ -189,9 +142,14 @@ var dynamicWorld = ee.ImageCollection('projects/nina/GIS_synergy/Extent/DW_globa
     return imgLabel
   });
 dynamicWorld = dynamicWorld.mosaic()
-dynamicWorld = dynamicWorld.updateMask(esri)
 Map.addLayer(dynamicWorld, lcVizParams, 'dynamicWorld preExport', 0);
 
+// updatemasks of LULC products to one another so that area calculations are comparable
+worldcover = worldcover.updateMask(esri).updateMask(dynamicWorld);
+dynamicWorld = dynamicWorld.updateMask(esri).updateMask(worldcover);
+esri = esri.updateMask(dynamicWorld).updateMask(worldcover);
+
+// Load other variables that serve for calculating accuracy per group
 //// Continents -------------------------------------------------------------------------
 var continents = ee.FeatureCollection('users/zandersamuel/Global_misc/continents');
 Map.addLayer(continents, {}, 'continents',0)
@@ -211,37 +169,45 @@ Map.addLayer(hsl, {min:0, max:3}, 'hsl',0)
 */
 
 //// Land cover proportions per grid ------------------------------------------------------------------
+// Remove (filter out) grid cells with no data (nulls)
 globalHexGrid = worldcover.reduceRegions(globalHexGrid, ee.Reducer.firstNonNull(), 5000)
   .filter(ee.Filter.notNull(['first']));
-Map.addLayer(globalHexGrid, {}, 'global grid filtered',0)
-print(globalHexGrid.size(),'globalHexGrid.size()')
+Map.addLayer(globalHexGrid, {}, 'global grid filtered',0)//visualise hex grid
+print(globalHexGrid.size(),'globalHexGrid.size()')//get number of valid cells (cells with data)
 
+// Get centroids for each grid cell
 var globalHexGridCentroids = globalHexGrid.map(function(ft){
   return ft.centroid(100)
 })
 
+// Remove (filter out) grid cells with no data (nulls)
 globalSquareGrid = worldcover.reduceRegions(globalSquareGrid, ee.Reducer.firstNonNull(), 5000)
   .filter(ee.Filter.notNull(['first']));
 Map.addLayer(globalSquareGrid, {}, 'global suare grid filtered',0)
 print(globalSquareGrid.size(), 'globalSquareGrid.size()')
 
+// global square grid is iterated through in for loop to extract data. By converting 
+// the square grid to a list an iterable is created.
 var gridList = globalSquareGrid 
   //.filterBounds(geometry) //// !!!!!! use for de-bugging and testing
   .toList(1000);
 
-// Change to length of list when ready to run all the exports
-for (var i = 0; i<1; i++){
+// Change to length of list (to size of square features (+1) in the aquare grid) 
+// when ready to run all the exports
+for (var i = 0; i<96; i++){
   var aoi = ee.Feature(gridList.get(i)).geometry()
   //Map.addLayer(aoi)
   var hexSelectNames = globalHexGridCentroids.filterBounds(aoi)
     .reduceColumns(ee.Reducer.toList(), ['seqnum']).get('list');
   var hexSelect = globalHexGrid.filter(ee.Filter.inList('seqnum', hexSelectNames));
   
+  //Create an imagecollection with the three global lc products. set a lc name property
   var lcImgCol = ee.ImageCollection([
     dynamicWorld.set('lc','dw'),
     worldcover.set('lc','wc'),
     esri.set('lc','esri')
     ]);
+  // for each lc product (image) create a band for each lc and rename to class name
   lcImgCol = lcImgCol.map(function(x){
     return x
       .eq([1,2,3,4,5,6,7,8,9])
@@ -249,22 +215,23 @@ for (var i = 0; i<1; i++){
       .rename(['BU','C','BG','G','SS','T','F','W','SI'])
       .copyProperties(x)
   });
-  
+  // for each of three lulc datasets (images, get the count of each of 9 lc (band) categories.
   var tableOut = lcImgCol.map(function(y){
     var tableInner = y.reduceRegions({
       collection: hexSelect, 
       reducer: ee.Reducer.count(), 
-      scale: 100,
-      tileScale: 2
+      scale: 10,
+      tileScale: 4
     });
     tableInner = tableInner.map(function(ft){ 
-      return ft.setGeometry(null).set('lc', y.get('lc'))
+      return ft.setGeometry(null).set('lc', y.get('lc'))//for each output create a dummy feature (no geometry) with lulc product name
     })
     return tableInner
-  }).flatten()
+  }).flatten()//format results for readability
   
-  print(tableOut.limit(2))
+  //print(tableOut.limit(2))
   
+  //create a unique csv export for each square grid cell
   Export.table.toDrive({
     collection: tableOut, 
     description: 'lc_areas_' + String(i) , 
@@ -272,7 +239,7 @@ for (var i = 0; i<1; i++){
   });
   
 }
-
+throw('s')
 // After running the script, you will have a whole lot of export tasks ready to run
 // Instead of clicking on each one individually, you can:
   // Copy the two functions below
@@ -288,7 +255,7 @@ for (var i = 0; i<1; i++){
 
 
 //// Site context - global validation sites ----------------------------------------------------------
-
+//set country attribute for validation centroids
 var continentsToExport = validationCentroids.filterBounds(continents)
   .map(function(ft){
     var val = continents.filterBounds(ft.geometry()).first();
@@ -301,6 +268,7 @@ Export.table.toDrive({
   fileFormat: 'CSV'
 })
 
+//set ecoregion attribute for validation centroids
 var ecoRegionsToExport = validationCentroids
   .map(function(ft){
     var val = ecoRegions.filterBounds(ft.geometry()).first();
@@ -313,6 +281,7 @@ Export.table.toDrive({
   fileFormat: 'CSV'
 })
 
+//set human settlement attribute for validation centroids
 var hslToExport = hsl.reduceRegions({
   collection: validationCentroids, 
   reducer: ee.Reducer.first(), 
@@ -325,8 +294,10 @@ Export.table.toDrive({
   description: 'hsl_global_val',
   fileFormat: 'CSV'
 })
-//// Site context - regional validation sites ----------------------------------------------------------
 
+// Repeat for LUCAS points
+//// Site context - regional validation sites ----------------------------------------------------------
+//set ecoregion attribute for regional validation points
 var ecoRegionsToExport = lucas.filterBounds(ecoRegions)
   .map(function(ft){
     var val = ecoRegions.filterBounds(ft.geometry()).first();
@@ -338,7 +309,7 @@ Export.table.toDrive({
   description: 'ecoregion_regional_val',
   fileFormat: 'CSV'
 })
-
+//set human settlement attribute for regional validation points
 var hslToExport = hsl.reduceRegions({
   collection: lucas, 
   reducer: ee.Reducer.first(), 
@@ -353,7 +324,7 @@ Export.table.toDrive({
 })
 
 
-//// Global validation pixel values ------------------------------------------------------------------
+//// Global validation pixel values - main --------------------------------------------------------------
 
 //validation = validation
 //  .filterBounds(geometry) //////// !!!!!  use for de-bugging and testing
@@ -361,11 +332,13 @@ Export.table.toDrive({
 var valList = validation.toList(1000);
 print(valList)
 
+// create a multiband image containing dynamic world validation images and 3 lulc datasets
 var pixelVals = validation.map(function(i){
   var lcStack = i.rename('groundTruth')
     .addBands(dynamicWorld.rename('dw'))
     .addBands(worldcover.rename('wc'))
     .addBands(esri.rename('esri'));
+  // For each dw lc pixel create a point to sample three lulc datasets 
   
   var outTable = lcStack.sample({
     region: i.geometry(), 
@@ -373,7 +346,7 @@ var pixelVals = validation.map(function(i){
     //factor: 1, 
     //seed: 123
     })
-  outTable = outTable.filter(ee.Filter.notNull(['groundTruth','dw']));
+  outTable = outTable.filter(ee.Filter.notNull(['groundTruth','dw']));//remove null dw points
   //outTable = outTable.map(function(x){ return x.set('dw_id', i.get('dw_id'))})
   
   return outTable
@@ -386,8 +359,74 @@ Export.table.toDrive({
   fileFormat: 'CSV'
 });
 
-//// European LUCAS pixel values -------------------------------------------
+//// Global validation pixel values - supplementary --------------------------------------------------------------
+// This repeats the above, except here Dynamic World composites are made in two different ways
+function probToCat(img){
+  var imgMax = img.reduce(ee.Reducer.max());
+  var imgCat = ee.Image(0)
+    .where(img.select('built').eq(imgMax), 1)
+    .where(img.select('crops').eq(imgMax), 2)
+    .where(img.select('bare').eq(imgMax), 3)
+    .where(img.select('grass').eq(imgMax), 4)
+    .where(img.select('shrub_and_scrub').eq(imgMax), 5)
+    .where(img.select('trees').eq(imgMax), 6)
+    .where(img.select('flooded_vegetation').eq(imgMax), 7)
+    .where(img.select('water').eq(imgMax), 8)
+    .where(img.select('snow_and_ice').eq(imgMax), 9)
+  imgCat = imgCat.selfMask()
+  return imgCat
+}
+var CLASS_NAMES = [
+    'water', 'trees', 'grass', 'flooded_vegetation', 'crops',
+    'shrub_and_scrub', 'built', 'bare', 'snow_and_ice'];
 
+//validation = validation
+//  .filterBounds(geometry) //////// !!!!!  use for de-bugging and testing
+
+var valList = validation.toList(1000);
+print(valList)
+
+var pixelVals = validation.map(function(i){
+  
+  var dwSupp = ee.ImageCollection('GOOGLE/DYNAMICWORLD/V1')
+    .filterBounds(i.geometry())
+    .filter(ee.Filter.calendarRange(2020,2020,'year'))
+    .select(CLASS_NAMES);
+  
+  // Method 2: mean reduction on probabilities and max probability
+  var dwMean = dwSupp.reduce(ee.Reducer.mean()).rename(CLASS_NAMES);
+  var dwMeanCat = probToCat(dwMean).rename('dwMeanCat')
+  
+  // Method 3: median reduction on probabilities and max probability
+  var dwMedian = dwSupp.reduce(ee.Reducer.median()).rename(CLASS_NAMES);
+  var dwMedianCat = probToCat(dwMedian).rename('dwMedianCat')
+  
+  
+  var lcStack = i.rename('groundTruth')
+    .addBands(dwMeanCat)
+    .addBands(dwMedianCat);
+  
+  var outTable = lcStack.sample({
+    region: i.geometry(), 
+    scale: 10, 
+    //factor: 1, 
+    //seed: 123
+    })
+  outTable = outTable.filter(ee.Filter.notNull(['groundTruth','dwMeanCat']));
+  //outTable = outTable.map(function(x){ return x.set('dw_id', i.get('dw_id'))})
+  
+  return outTable
+}).flatten();
+print(pixelVals.limit(10), 'dwSupp val tiles out supplementary')
+
+Export.table.toDrive({
+  collection: pixelVals, 
+  description: 'validation_global_DW_tiles_supplementary' , 
+  fileFormat: 'CSV'
+});
+
+//// European LUCAS pixel values - main -------------------------------------------
+// Create grid to iterate over and export data later
 var lucasExportGrid = ee.Image.random(123).multiply(1e6).toInt()
     .reduceToVectors({
       reducer: ee.Reducer.countEvery(),
@@ -398,18 +437,20 @@ var lucasExportGrid = ee.Image.random(123).multiply(1e6).toInt()
       crs: 'EPSG:4326'
     });
 
+// select ice classified points
 var lucasIce = lucas.filter(ee.Filter.eq('lc1', 'G50'));
 lucasIce = lucasIce.map(function(ft){
   return ft.set('lcLab', 'G50', 'lcNum', 9)
 })
-
+// filter out ice lc points
 lucas = lucas.filter(ee.Filter.neq('lc1', 'G50'));
 lucas = lucas.map(function(ft){
   var lcSimp = ee.String(ft.get('lc1')).slice(0,1)
   return ft.set('lcLab', lcSimp, 'lcNum', lcSimp)
 })
-lucas = lucas.filter(ee.Filter.neq('lcLab', ''));
+lucas = lucas.filter(ee.Filter.neq('lcLab', ''));//select points with labels
 
+// Reclassify lc names
 // Lookup can be found here: https://data.jrc.ec.europa.eu/dataset/f85907ae-d123-471f-a44a-8cca993485a2#dataaccess
 lucas = lucas.remap(
   ["A", "B", "C", "D", "E", "F", "G", "H"], 
@@ -420,19 +461,22 @@ lucas = lucas.merge(lucasIce)
 //lucas = lucas
 //  .filterBounds(geometry) //////// !!!!!  use for de-bugging and testing
 
+// create lc image with a band for each global lc product
 var lcStack = dynamicWorld.rename('dw')
   .addBands(worldcover.rename('wc'))
   .addBands(esri.rename('esri'));
 
+// convert grid to list (iterable)
 var lucasGridList = lucasExportGrid.toList(1000);
+print(lucasExportGrid.size(), 'lucasGridList size');
 
 // Change to length of list when ready to run all the exports
 for (var i = 0; i<2; i++){
   var aoi = ee.Feature(lucasGridList.get(i)).geometry();
   
-  var lucasSel = lucas.filterBounds(aoi);
+  var lucasSel = lucas.filterBounds(aoi);//select all points within grid cell
   
-  var lucasOut = lcStack.sampleRegions({
+  var lucasOut = lcStack.sampleRegions({//get lc properties for each of three lc products at points(within grid)
     collection: lucasSel, 
     scale: 10,
     geometries: true,
@@ -447,6 +491,49 @@ for (var i = 0; i<2; i++){
   });
 }
 
+//// European LUCAS pixel values - supplementary -------------------------------------------
+// This repeats the above, except here Dynamic World composites are made in two different ways
+
+var lucasGridList = lucasExportGrid.toList(1000);
+
+// Change to length of list when ready to run all the exports
+for (var i = 0; i<2; i++){
+  var aoi = ee.Feature(lucasGridList.get(i)).geometry();
+  
+  var lucasSel = lucas.filterBounds(aoi);
+  
+  var dwSupp = ee.ImageCollection('GOOGLE/DYNAMICWORLD/V1')
+    .filterBounds(aoi)
+    .filter(ee.Filter.calendarRange(2020,2020,'year'))
+    .select(CLASS_NAMES);
+  
+  // Method 2: mean reduction on probabilities and max probability
+  var dwMean = dwSupp.reduce(ee.Reducer.mean()).rename(CLASS_NAMES);
+  var dwMeanCat = probToCat(dwMean).rename('dwMeanCat')
+  
+  // Method 3: median reduction on probabilities and max probability
+  var dwMedian = dwSupp.reduce(ee.Reducer.median()).rename(CLASS_NAMES);
+  var dwMedianCat = probToCat(dwMedian).rename('dwMedianCat')
+  
+  var lcStack = dwMedianCat
+    .addBands(dwMeanCat);
+    
+  var lucasOut = lcStack.sampleRegions({
+    collection: lucasSel, 
+    scale: 10,
+    geometries: true,
+    tileScale: 4
+  });
+  //print(lucasOut.limit(10), 'lucas out')
+  
+  Export.table.toDrive({
+    collection: lucasOut, 
+    description: 'validation_european_LUCAS_pts_supplementary_' + String(i) , 
+    fileFormat: 'CSV'
+  });
+  
+  
+}
 
 
 /*
@@ -454,7 +541,7 @@ for (var i = 0; i<2; i++){
 */
 
 //// Global extent exports --------------------------------------------------------------
-
+// Export images at a reduced resolution
 var dwToExport = dynamicWorld
   .reduceResolution(ee.Reducer.mode(), true, 100)
   .reproject(worldcover.projection().atScale(2000));
